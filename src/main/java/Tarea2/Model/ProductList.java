e4496ed6fe89b0f5ae3a08714f4e8dc9bee46154
Meyer <meyermontagner@gmail.com>
2018-10-22 00:11:32 +0200

    Guardados cambios en interface

--- src/main/java/Tarea2/Model/ProductList.java
+++ src/main/java/Tarea2/Model/ProductList.java
@@ -3,4 +3,8 @@ package Tarea2.Model;
 import java.util.List;
 
 public interface ProductList {
+    List<Product> getProductList();
+    void addProduct(Product product);
+    Product getProduct(String code);
+    Product removeProduct(String code);
 }
